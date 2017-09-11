5257e85620205a5faca7e276e62e6f56afeb9196
Eduardo <e.aboy@sinteac.es>
2017-09-12 00:00:17 +0200

    Ejemplo de conexión con mysql

---
+++ ejemplos/ejemplo_mysql/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// cargamos el driver mysql
+
+const mysql = require('mysql');
+
+// creamos una conexión 
+
+const connection = mysql.createConnection({
+    host:'didimo.es',
+    user: 'usuariocurso',
+    password: 'us3r',
+    database: 'cursonode'
+});
+
+// conectamos
+
+connection.connect();
+
+// lanzamos una query
+
+connection.query('SELECT * FROM agentes', (err, rows, fields) => {
+
+    if(err) {
+        console.log('Hubo un error', err);
+        process.exit(1);
+        return;
+    }
+
+    for(let i = 0 ; i < rows.length ; i++ ) {
+        const agente = rows[i];
+        console.log(agente.idagentes, agente.name, agente.age);        
+    }
+});
+
+// pintamos los resultados

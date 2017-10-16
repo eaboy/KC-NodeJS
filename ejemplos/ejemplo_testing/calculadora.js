928dd72400216d059851b1a4ed4cbdec9311e3ca
Eduardo <e.aboy@sinteac.es>
2017-10-17 00:04:24 +0200

    Contiunues the testing.

--- ejemplos/ejemplo_testing/calculadora.js
+++ ejemplos/ejemplo_testing/calculadora.js
@@ -5,4 +5,11 @@ Calculadora.prototype.sum = function (a = 0, b = 0) {
     return a + b;
 };
 
+Calculadora.prototype.sumAfter = function (a, b, ms, callback) {
+    setTimeout(() => {
+        const result = this.sum(a, b);
+        callback(result);
+    }, ms);
+};
+
 module.exports = Calculadora;
--- ejemplos/ejemplo_testing/calculadora.spec.js
+++ ejemplos/ejemplo_testing/calculadora.spec.js
@@ -1,20 +1,44 @@
 const Calculadora = require('./calculadora');
 const chai = require('chai');
 const expect = chai.expect;
+const sinon = require('sinon');
 
-describe('calculadora', function() {
+describe('calculadora', function () {
 
     let calculadora;
 
-    beforeEach(function() {
+    beforeEach(function () {
         calculadora = new Calculadora();
     });
 
-    it('sum() should return 0 if no arguments are passed in', function() {
+    it('sum() should return 0 if no arguments are passed in', function () {
         expect(calculadora.sum()).to.equal(0);
     });
 
-    it('sum() should return the sum of 10 and 15', function() {
-        expect(calculadora.sum(10,15)).to.equal(25);
+    it('sum() should return the sum of 10 and 15', function () {
+        expect(calculadora.sum(10, 15)).to.equal(25);
     });
+
+    it('sumAfter() should execute callback function with the result', function (done) {
+        // this.timeout(4000); le indica a mocha que debe esperar 4 segundos a que termine el test en vez de los 2 por defecto
+        // Hacer mock de sum para que no dependa de el con sinon
+        sinon.stub(calculadora, 'sum').returns(20);
+        calculadora.sumAfter(10, 10, 10, result => {
+            expect(result).to.be.not.undefined;
+            calculadora.sum.restore(); // Limpiamos el stub
+            done();
+        });
+    });
+
+    it('sumAfter() should call sum() to obtain the result', function (done) {
+        // this.timeout(4000); le indica a mocha que debe esperar 4 segundos a que termine el test en vez de los 2 por defecto
+        // Hacer mock de sum para que no dependa de el con sinon
+        sinon.stub(calculadora, 'sum').returns(20);
+        calculadora.sumAfter(10, 10, 10, result => {
+            expect(calculadora.sum.firstCall.args).to.deep.equal([10, 10]);
+            calculadora.sum.restore(); // Limpiamos el stub
+            done();
+        });
+    });
+
 });
